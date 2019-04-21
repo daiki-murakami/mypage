1907db6170410acae4fc35a64fe096003e308692
daiki-murakmai <muranokami1@gmail.com>
2019-04-21 23:15:44 +0900

    pageのバリデーション

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,7 @@
 class Page < ApplicationRecord
+
+    validates :title, presence: true, length: {maximum: 50 }
+    validates :tag, presence: true, length: {maximum: 50 }
+    validates :content, presence: true
+
 end
--- test/models/page_test.rb
+++ test/models/page_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class PageTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @page = Page.new(title: "Example Page", tag: "others", content: "test")
+  end
+
+  test "should be valid" do
+    assert @page.valid?
+  end
+
+  test "title should be present" do
+    @page.title = "   "
+    assert_not @page.valid?
+  end
+
+  test "tag should be present" do
+    @page.tag = "   "
+    assert_not @page.valid?
+  end
+
+  test "content should be present" do
+    @page.content = "   "
+    assert_not @page.valid?
+  end
+
+  test "name should not be too long" do
+    @page.title = "a" * 51
+    assert_not @page.valid?
+  end
+
+  test "email should not be too long" do
+    @page.tag = "a" * 51
+    assert_not @page.valid?
+  end
 end
