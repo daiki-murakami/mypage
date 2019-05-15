6d2d43a9ee8faafbfb4c52754f9cebffa1952f28
daiki-murakmai <muranokami1@gmail.com>
2019-05-16 00:19:10 +0900

    indexの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/index'
   get 'pages/new'
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-
-  resources :pages
   get '/create', to: 'pages#new'
   post '/create',  to: 'pages#create'
+  root 'static_pages#home'
+  get 'static_pages/home'
+  get 'static_pages/profile'
+
+  resources :pages
 end
